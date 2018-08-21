c15451e4eeeaafb45f88006c05e47f9cb0dde900
Joshua Liu <liujoshua@users.noreply.github.com>
2018-08-21 13:18:38 -0700

    Dokka for javadoc generation for Java and Kotlin

--- api/src/main/java/org/researchstack/result/Result.kt
+++ api/src/main/java/org/researchstack/result/Result.kt
@@ -1,5 +1,11 @@
 package org.researchstack.result
 
+/**
+ * A Result.
+ */
 interface Result {
+    /**
+     * Returns identifier for this result.
+     */
     fun getIdentifier(): String
 }
---
+++ sample-app/src/main/java/org/researchstack/sampleapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package org.researchstack.sampleapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.fab
+import kotlinx.android.synthetic.main.activity_main.toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
