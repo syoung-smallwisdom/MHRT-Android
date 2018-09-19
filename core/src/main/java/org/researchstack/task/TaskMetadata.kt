b871b018e0522041e0b40b88bb964b265e42a828
syoung-smallwisdom <shannon.young@sagebase.org>
2018-09-19 15:29:42 -0700

    Convert TaskMetadata to a data class

--- core/src/main/java/org/researchstack/task/TaskMetadata.kt
+++ core/src/main/java/org/researchstack/task/TaskMetadata.kt
@@ -2,14 +2,8 @@ package org.researchstack.task
 
 import org.researchstack.api.task.TaskMetadata
 
-class TaskMetadata : TaskMetadata {
-    override val identifier: String
-
-    override var title: String? = null
-
-    override var subtitle: String? = null
-
-    constructor(identifier: String) {
-        this.identifier = identifier
-    }
+data class TaskMetadata(
+        override val identifier: String,
+        override val title: String? = null,
+        override val subtitle: String? = null) : TaskMetadata {
 }
